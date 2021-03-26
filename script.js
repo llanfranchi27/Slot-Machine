74c2d697206dc8542c12a66931ab6f4779e82c3a
luismateyl <luismateyl23@gmail.com>
2021-03-25 20:12:54 -0400

    Updated css

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 window.onload=init;
 
+<<<<<<< Updated upstream
 let credit; 
+=======
+const images = [
+  "imgs/1.png",
+  "imgs/2.png",
+  "imgs/python-icon.png",
+  "imgs/ruby-icon.png",
+  "imgs/coin2.png",
+];
+
+const currentImages = [];
+>>>>>>> Stashed changes
 
 const images=["java-icon.png", "jquery-icon.png", "js-icon.png", "php-icon.png", "python-icon.png", "ruby-icon.png", "coin2.png"];
 
@@ -48,13 +60,29 @@ function compareImages() {
 
 function newCredits() {
 
+<<<<<<< Updated upstream
+=======
+function getRandomImg() {
+  let randomIdx = Math.floor(Math.random() * images.length);
+  return images[randomIdx];
+>>>>>>> Stashed changes
 }
 
 function showMessage() {
 
 }
 
+<<<<<<< Updated upstream
 function closingButton(){
+=======
+function compareImage() {
+  messageId.textContent = "";
+  if (leftImage.src === middleImage.src && middleImage.src === rightImage.src){
+      messageId.textContent = "You have won!";
+  } else {
+    messageId.textContent = "You have lost!";
+  }
+>>>>>>> Stashed changes
 
 }
 
